d37b1d00782fa9f4aa21123c458c2fb1718e64d6
Amber Mills <amberdmills@outlook.com>
2021-10-15 13:54:20 -0500

    commit

---
+++ Fed1/javaScript/nav.js
@@ -0,0 +1,16 @@
+﻿
+
+
+
+function Link1() {
+    window.location.href = "Link1";
+}
+function Link2() {
+    window.location.href = "Link2";
+}
+function Link2.1(){
+    window.location.href = "Link2.1";
+}
+function Link2.2(){
+    window.location.href = "Link2.2";
+}
